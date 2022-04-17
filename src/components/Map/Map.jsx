4a3a812d534192ab2bb6d930959c9af9683839c5
Justinnn07 <justinv2006@gmail.com>
2022-04-17 13:24:24 +0530

    fixed bugs :hammer:

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,45 +1,9 @@
 import React from "react";
 import Map, { Source, Layer } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { heatMapLayer, dataLayer } from "../../data/mapLayers";
 
 const Maps = ({ data, type }) => {
-  const dataLayer = {
-    id: "room-extrusion",
-    type: "fill-extrusion",
-    source: "floorplan",
-
-    paint: {
-      "fill-extrusion-color": ["get", "color"],
-      "fill-extrusion-height": ["get", "height"],
-      "fill-extrusion-base": ["get", "base_height"],
-      "fill-extrusion-opacity": 0.5,
-    },
-  };
-
-  const heatMapLayer = {
-    id: "heat-map",
-    type: "heatmap",
-    paint: {
-      "heatmap-color": [
-        "interpolate",
-        ["linear"],
-        ["heatmap-density"],
-        0,
-        "rgba(33,102,172,0)",
-        0.2,
-        "rgb(103,169,207)",
-        0.4,
-        "rgb(209,229,240)",
-        0.6,
-        "rgb(253,219,199)",
-        0.8,
-        "rgb(239,138,98)",
-        1,
-        "rgb(178,24,43)",
-      ],
-    },
-  };
-
   return (
     <Map
       initialViewState={{
