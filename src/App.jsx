c465440b39ad8c95e06e1b9a53c3e2c5119ef18e
Justinnn07 <justinv2006@gmail.com>
2022-04-17 10:53:13 +0530

    fixed bugs :hammer:

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import Maps from "./components/Map/Map";
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
-import * as React from "react";
+import React from "react";
 import { styled, alpha } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import MenuIcon from "@mui/icons-material/Menu";
-import { MenuItem, Select } from "@mui/material";
+import { Menu } from "@mui/icons-material";
+import {
+  MenuItem,
+  Select,
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+} from "@mui/material";
 
 const RightContainer = styled("div")(({ theme }) => ({
   position: "relative",
@@ -38,7 +41,7 @@ const Header = ({ selectData, setSelectData, floor, setFloor }) => {
             aria-label="open drawer"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <Menu />
           </IconButton>
           <Typography
             variant="h6"
