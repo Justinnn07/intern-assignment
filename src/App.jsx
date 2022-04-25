d8b6dffd858bfdc8e7729924c9e0304ffc04d483
Justinnn07 <justinv2006@gmail.com>
2022-04-25 15:57:36 +0530

    optimised code :fire:

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import Maps from "./components/Map/Map";
 import { getDataLayer } from "./hooks/Hooks";
